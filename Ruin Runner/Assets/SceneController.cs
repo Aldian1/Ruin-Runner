d7a39953445727d14432708c206deeb3ea8fc03d
Aldian1 <Aldian@hotmail.co.uk>
2016-06-07 12:39:03 +0100

    Added:
    New assets
    lighting redone
    all sprites now have a material on them
    jump mechanic

--- Ruin Runner/Assets/SceneController.cs
+++ Ruin Runner/Assets/SceneController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SceneController : MonoBehaviour {
+public class SceneController : MonoBehaviour
+{
 
     public GameObject[] currentpillars;
 
@@ -15,33 +16,53 @@ public class SceneController : MonoBehaviour {
 
     public float difficultyMultiplier;
 
-    public GameObject doublepillar,bottompillar;
-	// Use this for initialization
-	void Start () {
+    public GameObject doublepillar, bottompillar;
+
+    public GameObject Bow,Player;
+
+    public float speed, jumppower,rightjumppower;
+
+    private Rigidbody2D rb; 
+
+  
+    // Use this for initialization
+    void Start()
+    {
+        rb = Player.GetComponent<Rigidbody2D>();
         //Load pillars from resources in future
 
         //Spawn our starting area of x tiles
-     
-        for(int i = 0; i < StartingAreaSize; i++)
+
+        for (int i = 0; i < StartingAreaSize; i++)
         {
             //setting the random makes the platforms not concurrent but makes them look slightly spaced. 
-            int u = Random.Range(2,6);
+            int u = Random.Range(2, 6);
             int foil = Random.Range(0, 10);
 
-      
-            GameObject go = Instantiate(bottompillar, new Vector3(CurrentPositionOfLastPlatform.position.x + u, Random.Range(-2, 0), -4.5F), Quaternion.identity) as GameObject;
-           CurrentPositionOfLastPlatform = go.transform;
 
-            if(foil == 1 || foil == 2)
+            GameObject go = Instantiate(bottompillar, new Vector3(CurrentPositionOfLastPlatform.position.x + u, Random.Range(-2, 0), -4.5F), Quaternion.identity) as GameObject;
+            CurrentPositionOfLastPlatform = go.transform;
+
+            if (foil == 1 || foil == 2)
             {
                 go.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = foilagesrites[Random.Range(0, foilagesrites.Length)];
             }
 
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player.transform.Translate(Vector3.right * speed);
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            rb.AddForce(Vector2.up * jumppower);
+            rb.AddForce(Vector2.right * rightjumppower);
+        }
+
+    }
+
+
 }
