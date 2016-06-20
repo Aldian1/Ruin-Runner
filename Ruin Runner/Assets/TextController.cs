8d74b2c36bbec78009fb535e0f568e36b6643d20
Aldian1 <Aldian@hotmail.co.uk>
2016-06-20 22:15:07 +0100

    Alpha 0.4

--- Ruin Runner/Assets/TextController.cs
+++ Ruin Runner/Assets/TextController.cs
@@ -7,13 +7,23 @@ public class TextController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         StartCoroutine("AnimationTimer");
+        Debug.Log("aaa");
 	}
+    void OnEnable()
+    {
+        StartCoroutine("AnimationTimer");
+    }
+
+        void Awake()
+    { StartCoroutine("AnimationTimer"); }
+
     IEnumerator AnimationTimer()
     {
         yield return new WaitForSeconds(.50F);
         GetComponent<Text>().CrossFadeAlpha(0, 1, true);
         yield return new WaitForSeconds(1);
-        this.gameObject.SetActive(false);
         this.enabled = false;
+        this.gameObject.SetActive(false);
+        
     }
 }
---
+++ Ruin Runner/Assets/UrlPopper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UrlPopper : MonoBehaviour {
+
+
+
+    public void UrlPop(string URL)
+    {
+        Application.OpenURL(URL);
+    }
+}
