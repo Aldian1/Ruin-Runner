b0c1ac49d8777504dc2539beaa1636a4ed73078a
Aldian1 <Aldian@hotmail.co.uk>
2016-06-24 13:38:21 +0100

    Revert "Alpha 0.4"
    This reverts commit 8d74b2c36bbec78009fb535e0f568e36b6643d20.

--- Ruin Runner/Assets/TextController.cs
+++ Ruin Runner/Assets/TextController.cs
@@ -7,23 +7,13 @@ public class TextController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         StartCoroutine("AnimationTimer");
-        Debug.Log("aaa");
 	}
-    void OnEnable()
-    {
-        StartCoroutine("AnimationTimer");
-    }
-
-        void Awake()
-    { StartCoroutine("AnimationTimer"); }
-
     IEnumerator AnimationTimer()
     {
         yield return new WaitForSeconds(.50F);
         GetComponent<Text>().CrossFadeAlpha(0, 1, true);
         yield return new WaitForSeconds(1);
-        this.enabled = false;
         this.gameObject.SetActive(false);
-        
+        this.enabled = false;
     }
 }
